b48417f82e12066f7d5c74fd2dbe70233245ae8b
Harsh639703 <harsh639703@gmail.com>
2024-05-04 23:07:58 +0530

    added array learning

---
+++ 02_basics/01_array.js
@@ -0,0 +1,29 @@
+// const arr = [1,2,3,4,5]
+
+// console.log(arr);
+const arr1= new Array(1,2,88,4,5)//another way of declaring array
+// console.log(arr1);
+
+// arr1.push(989)
+// // console.log(arr1);
+// arr1.pop()
+// // console.log(arr1);
+
+// arr1.unshift(9)
+// console.log(arr1);
+// arr1.shift()
+// console.log(arr1);
+
+// console.log(arr1.includes(99));//include returns the true or false
+// console.log(arr1.includes(5));
+
+
+// const arrw = arr1.join()
+// console.log(arrw);
+// console.log(typeof arrw);
+
+const myn1 = arr1.slice(1,3)
+console.log(myn1);
+const myn2 = arr1.splice(1,3)//it manipulate the complete array 
+console.log(myn2);
+
