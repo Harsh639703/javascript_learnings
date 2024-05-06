4e7664412bef20301c7f5b83e80371e1ae08b198
Harsh639703 <harsh639703@gmail.com>
2024-05-06 23:12:13 +0530

    learned about the ternary operators

---
+++ 04_controlflow/03_truthy.js
@@ -0,0 +1,32 @@
+//false values
+
+// false ,0 ,-0,BigInt 0n, null undefined, NaN//these all values returns false
+
+//Truthy Values
+
+//"0",'false',function(), {}, [], //these all values return the true value
+
+// const my ={}
+
+// if(Object.keys(my.length===0)){
+//     console.log("object is empty");
+// }
+
+//nullish coelscing operator (??):
+
+// let val1
+
+// val1 = 5 ?? 10
+// val1 = null ?? 14
+// val1 = undefined ?? 114
+
+// val1 = null ?? 102 ?? 20
+// console.log(val1);
+
+
+//ternary operator
+
+
+const  a = 100
+a>=80 ? console.log("han bhai greater than 90 hai "): console.log(`nahi hai yrrr`);
+
