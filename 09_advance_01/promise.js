52a4a3db7f93358aab39692cb0eee67ce548217c
Harsh639703 <harsh639703@gmail.com>
2024-05-13 18:09:42 +0530

    learned about the fetch and how it works

--- 09_advance_01/promise.js
+++ 09_advance_01/promise.js
@@ -36,24 +36,74 @@
 // })
 
 
-const promiseFour = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if (!error) {
-            resolve({username: "HARSH", password: "123"})
-        } else {
-            reject('ERROR: Something went wrong')
-        }
-    }, 2000)
-})
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true
+//         if (!error) {
+//             resolve({username: "HARSH", password: "123"})
+//         } else {
+//             reject('ERROR: Something went wrong')
+//         }
+//     }, 2000)
+// });
 
- promiseFour
- .then((user) => {
-    console.log(user);
-    return user.username
-}).then((username) => {
-    console.log(username);
-}).catch(function(error){
-    console.log(error);
-}).finally(() => console.log("The promise is either resolved or rejected"))
+//  promiseFour
+//  .then((user) => {
+//     console.log(user);
+//     return user.username
+// }).then((username) => {
+//     console.log(username);
+// }).catch(function(error){
+//     console.log(error);
+// }).finally(() => console.log("The promise is either resolved or rejected"))
+
+// const promisefive = new Promise(function(resolve,reject){
+//       setTimeout(function(){
+//              let error = false
+//              if(!error){
+//                 resolve({username:"Harsh",email:"harsh639703@gmail.com"}) 
+//              }else{
+//                 reject('ERROR: JS went error')
+//              }
+             
+//       },2000)
+// })
+
+// async function consumefive(){
+//     try{
+//         const response = await promisefive
+//         console.log(response);
+//     }catch(error){
+//         console.log(error);
+//     }
+   
+// }
+// consumefive()
+
+
+// async function getallusers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E : ",error);
+//     }
+   
+// }
+
+// getallusers()
+
+
+
+ fetch('https://jsonplaceholder.typicode.com/users')
+ .then((response)=>{
+         return response.json()
+ }).then((data)=>{
+    console.log(data);
+ }).catch((error)=>{
+    return console.log(error);
+ })
+
+
 
